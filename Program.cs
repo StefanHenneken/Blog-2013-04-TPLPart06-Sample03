f8a6a0440223c807b67d4eb05bf6468797087508
Stefan Henneken <stefan.henneken@live.com>
2017-06-07 22:09:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sample03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+        public void Run()
+        {
+            Console.WriteLine("Start Run");
+
+            double result01 = 0;
+            for (int i = 1; i < 10000; i++)
+            {
+                result01 += DoSomeWork(i);
+            }
+            Console.WriteLine("Result: {0}", result01);
+
+            double result02 = 0;
+            object locker = new Object();
+            int inits = 0;
+            int aggregates = 0;
+            Parallel.For<double>(1, 10000,
+            () =>
+            {
+                Interlocked.Increment(ref inits);
+                double tls = 0.0;
+                return tls;
+            },
+            (i, pls, tls) =>
+            {
+                tls += DoSomeWork(i);
+                return tls;
+            },
+            (tls) =>
+            {
+                Interlocked.Increment(ref aggregates);
+                lock (locker)
+                {
+                    result02 += tls;
+                }
+            });
+            Console.WriteLine("Result: {0}", result02);
+            Console.WriteLine("inits: {0}   aggregates: {1}", inits, aggregates);
+
+            Console.WriteLine("End Run");
+            Console.ReadLine();
+        }
+        private double DoSomeWork(int index)
+        {
+            return Math.Sin(index) + Math.Sqrt(index) * Math.Pow(index, 0.14);
+        }
+    }
+}
